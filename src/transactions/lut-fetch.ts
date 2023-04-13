0f213d54f845c7f6fe63a1beb95253b1f7cc89a9
int <int_eth@proton.me>
2023-04-12 20:32:25 -0500

    feat(tx): fetch and parse lut from onchain

---
+++ src/transactions/lut-fetch.ts
@@ -0,0 +1,22 @@
+import { Connection, PublicKey } from '@solana/web3.js';
+
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const main = async () => {
+  const connection = new Connection(process.env.OFFICIAL_SOL_DEV_HTTPS!);
+
+  const lutAddress = new PublicKey(process.env.LUT_DEVNET_1!);
+
+  const lookupTableAccount = await connection
+    .getAddressLookupTable(lutAddress)
+    .then((res) => res.value);
+
+  for (let i = 0; i < lookupTableAccount!.state.addresses.length; i += 1) {
+    const address = lookupTableAccount!.state.addresses[i];
+    console.log(i, address.toBase58());
+  }
+};
+
+main();
