70e82161166c8fa75228465ec39c4b5eb4b8104f
int <int_eth@proton.me>
2023-04-17 10:09:28 -0500

    feat(token): create user associated token account

---
+++ src/token/associated-account.ts
@@ -0,0 +1,45 @@
+import {
+  createAssociatedTokenAccountInstruction,
+  getAssociatedTokenAddress,
+} from '@solana/spl-token';
+import {
+  Connection,
+  PublicKey,
+  sendAndConfirmTransaction,
+  Transaction,
+} from '@solana/web3.js';
+import * as dotenv from 'dotenv';
+
+import { loadWallet } from 'keypair';
+
+dotenv.config();
+
+// user associated token account
+// deterministically derived
+
+const main = async () => {
+  const connection = new Connection(process.env.OFFICIAL_SOL_DEV_HTTPS!);
+  const wallet = loadWallet();
+
+  const tokenMint = new PublicKey(process.env.TOKEN_MINT_1!);
+
+  const associatedTokenAddress = await getAssociatedTokenAddress(
+    tokenMint,
+    wallet.publicKey
+  );
+
+  const createTokenAccountIx = createAssociatedTokenAccountInstruction(
+    wallet.publicKey,
+    associatedTokenAddress,
+    wallet.publicKey,
+    tokenMint
+  );
+
+  const tx = new Transaction().add(createTokenAccountIx);
+
+  const sig = await sendAndConfirmTransaction(connection, tx, [wallet]);
+
+  console.log(sig);
+};
+
+main();
